dae5d3f01f843cc9c27c0cd09944b5d34ea21373
ahamefulachibundu <ahamefulachibundu@gmail.com>
2026-08-26 11:35:22 +0100

    feat: add medication log routes

--- backend/src/routes/medicationLog.route.js
+++ backend/src/routes/medicationLog.route.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const {
+    getMedicationHistory,
     markDoseAsTaken,
     markDoseAsSkipped
 } = require('../controllers/medicationLog.controller');
 
 const router = express.Router();
+
+router.get("/medication-history/:profileId", getMedicationHistory);
 
 router.patch ('/medication-logs/:id/taken', markDoseAsTaken);
 
